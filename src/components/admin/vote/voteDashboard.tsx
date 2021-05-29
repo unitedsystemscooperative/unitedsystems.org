45b411c915ea9048aaf054a2494a858593824e9e
Zachary Bryant <Zachary@admiralfeb.net>
2021-05-29 01:52:10 -0500

    feat: vote assistant complete

--- src/components/admin/vote/voteDashboard.tsx
+++ src/components/admin/vote/voteDashboard.tsx
@@ -1,23 +1,60 @@
 import { Container, makeStyles, Paper, Typography } from '@material-ui/core';
+import { useCMDRs } from 'hooks/useCmdrs';
+import { Rank } from 'models/admin/ranks';
+import { IVoter } from 'models/admin/voter';
+import { useEffect, useState } from 'react';
 import { Voter } from './voter';
 
-const useStyles = makeStyles((_) => ({
+const useStyles = makeStyles((theme) => ({
   title: {
     textAlign: 'center',
   },
+  paper: {
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+  },
 }));
 
-const voters = ['voter', 'voter1', 'voter2'];
 export const VoteDashboard = () => {
   const classes = useStyles();
+  const { cmdrs, loading } = useCMDRs();
+  const [voteInfo, setVoteInfo] = useState<IVoter[]>([]);
+
+  useEffect(() => {
+    if (window) {
+      const store: IVoter[] | null = JSON.parse(
+        window.localStorage.getItem('voters')
+      );
+      if (store && store.length > 0) {
+        setVoteInfo(store);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!loading && voteInfo.length < 1) {
+      const hcCmdrs = cmdrs.members.filter(
+        (c) => c.rank <= Rank.Captain && c.isDeleted !== true
+      );
+      let hcVoters: IVoter[] = [];
+      for (const voter of hcCmdrs) {
+        hcVoters = [
+          ...hcVoters,
+          { name: voter.cmdrName.toUpperCase(), hasVoted: null },
+        ];
+      }
+      setVoteInfo(hcVoters);
+    }
+  }, [loading, cmdrs.members, voteInfo]);
+
   return (
     <>
       <Typography variant="h3" className={classes.title}>
-        Vote Dashboard
+        Vote Assistant
       </Typography>
-      <Container maxWidth="sm" component={Paper}>
-        {voters.map((voter) => (
-          <Voter key={voter} />
+      <Container maxWidth="xs" component={Paper} className={classes.paper}>
+        {voteInfo.map((voter) => (
+          <Voter key={voter.name} voter={voter} />
         ))}
       </Container>
     </>
--- src/components/admin/vote/voter.tsx
+++ src/components/admin/vote/voter.tsx
@@ -1,5 +1,43 @@
-import { Typography } from '@material-ui/core';
+import { makeStyles, Typography } from '@material-ui/core';
+import { Check, Clear, Remove } from '@material-ui/icons';
+import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab';
+import { IVoter } from 'models/admin/voter';
+import React, { useState, MouseEvent } from 'react';
 
-export const Voter = () => {
-  return <Typography>Voter</Typography>;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  expand: {
+    flexGrow: 1,
+  },
+  text: {},
+}));
+
+export const Voter = ({ voter }: { voter: IVoter }) => {
+  const classes = useStyles();
+  const [vote, setVote] = useState<boolean | null>(null);
+
+  const handleVote = (_: MouseEvent<HTMLElement>, newVote: boolean | null) => {
+    setVote(newVote);
+  };
+  return (
+    <div className={classes.root}>
+      <Typography>{voter.name}</Typography>
+      <div className={classes.expand} />
+      <ToggleButtonGroup value={vote} exclusive onChange={handleVote}>
+        <ToggleButton value={true}>
+          <Check />
+        </ToggleButton>
+        <ToggleButton value={null}>
+          <Remove />
+        </ToggleButton>
+        <ToggleButton value={false}>
+          <Clear />
+        </ToggleButton>
+      </ToggleButtonGroup>
+    </div>
+  );
 };
