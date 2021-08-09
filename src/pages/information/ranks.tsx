d84b04fe1e3d086af13aea34e78c0652ab4e2bea
Zachary Bryant <Zachary@admiralfeb.net>
2021-08-08 22:48:10 -0500

    refactor: cleanup rank page deps

--- src/pages/information/ranks.tsx
+++ src/pages/information/ranks.tsx
@@ -1,20 +1,8 @@
-import { Container, makeStyles, Paper, Typography } from '@material-ui/core';
 import { PrimaryLayout } from 'components/layouts';
-import BgsMarkdown from 'components/information/bgs.mdx';
 import Head from 'next/head';
 import { Ranks } from 'components/information/ranks/ranks';
 
-const useStyles = makeStyles((theme) => ({
-  center: {
-    textAlign: 'center',
-  },
-  paper: {
-    padding: theme.spacing(2),
-  },
-}));
-
 const RanksPage = () => {
-  const classes = useStyles();
   return (
     <>
       <Head>
