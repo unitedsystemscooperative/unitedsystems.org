2416aba3515d8806e2471b05aee016d02b76dbe5
Zachary Austin <Zachary@admiralfeb.net>
2021-09-17 17:54:23 -0500

    refactor: mv theme declaration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,18 @@
-import { CssBaseline, ThemeProvider, Theme, StyledEngineProvider } from '@mui/material';
+import DateAdapter from '@date-io/moment';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import { MDXProvider } from '@mdx-js/react';
+import {
+  CssBaseline,
+  StyledEngineProvider,
+  ThemeProvider,
+} from '@mui/material';
+import { mdComponents } from 'components/markdown/markdownComponents';
 import { Provider } from 'next-auth/client';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { SnackbarProvider } from 'notistack';
 import React, { useEffect } from 'react';
 import { theme } from 'theme';
-import { MDXProvider } from '@mdx-js/react';
-import { mdComponents } from 'components/markdown/markdownComponents';
-import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-import DateAdapter from '@date-io/moment';
-
-
-declare module '@mui/styles/defaultTheme' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  interface DefaultTheme extends Theme {}
-}
-
 
 function USCApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
@@ -24,40 +21,42 @@ function USCApp({ Component, pageProps }: AppProps) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
   }, []);
-  return <>
-    <Head>
-      <title>United Systems Cooperative</title>
-      <meta
-        name="description"
-        content="Web site of the United Systems Cooperative"
-      />
-      <meta
-        name="viewport"
-        content="minimum-scale=1, initial-scale=1, width=device-width"
-      />
-      <meta name="og:image" content="https://unitedsystems.org/uscLogo.png" />
-      <link rel="icon" href="/uscLogo.png" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Cinzel&display=swap"
-        rel="stylesheet"
-      />
-    </Head>
-    <Provider session={pageProps.session}>
-      <MuiPickersUtilsProvider utils={DateAdapter}>
-        <SnackbarProvider maxSnack={3}>
-          <StyledEngineProvider injectFirst>
-            <ThemeProvider theme={theme}>
-              <CssBaseline />
-              <MDXProvider components={mdComponents}>
-                <Component {...pageProps} />
-              </MDXProvider>
-            </ThemeProvider>
-          </StyledEngineProvider>
-        </SnackbarProvider>
-      </MuiPickersUtilsProvider>
-    </Provider>
-  </>;
+  return (
+    <>
+      <Head>
+        <title>United Systems Cooperative</title>
+        <meta
+          name="description"
+          content="Web site of the United Systems Cooperative"
+        />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        <meta name="og:image" content="https://unitedsystems.org/uscLogo.png" />
+        <link rel="icon" href="/uscLogo.png" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cinzel&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Provider session={pageProps.session}>
+        <MuiPickersUtilsProvider utils={DateAdapter}>
+          <SnackbarProvider maxSnack={3}>
+            <StyledEngineProvider injectFirst>
+              <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <MDXProvider components={mdComponents}>
+                  <Component {...pageProps} />
+                </MDXProvider>
+              </ThemeProvider>
+            </StyledEngineProvider>
+          </SnackbarProvider>
+        </MuiPickersUtilsProvider>
+      </Provider>
+    </>
+  );
 }
 
 export default USCApp;
