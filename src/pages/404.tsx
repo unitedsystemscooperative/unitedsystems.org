a5a6c2063d4e17d3cf275ea3e72b068df44cb010
Zachary Austin <Zachary@admiralfeb.net>
2021-09-17 21:33:03 -0500

    chore: update eslint

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,7 +2,9 @@ import { Container, Link, Paper, Typography } from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
 import { PrimaryLayout } from 'components/layouts/primary';
 import Head from 'next/head';
+import Image from 'next/image';
 import NextLink from 'next/link';
+import errorImg from 'public/img/404.jpg';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,22 +24,22 @@ export const NotFound = () => {
       <Head>
         <title>United Systems Cooperative</title>
         <meta
-          name='description'
-          content='Web site of the United Systems Cooperative'
+          name="description"
+          content="Web site of the United Systems Cooperative"
         />
       </Head>
       <PrimaryLayout>
-        <Container maxWidth='lg' className={classes.root}>
+        <Container maxWidth="lg" className={classes.root}>
           <Paper>
-            <Typography variant='h3'>Mistakes were made</Typography>
-            <Typography variant='subtitle1'>
+            <Typography variant="h3">Mistakes were made</Typography>
+            <Typography variant="subtitle1">
               You were hyperdicted... escape destruction by going{' '}
-              <NextLink href='/home' passHref>
+              <NextLink href="/home" passHref>
                 <Link>home</Link>
               </NextLink>
               .
             </Typography>
-            <img className={classes.img} alt='404 Error' src='/img/404.jpg' />
+            <Image className={classes.img} alt="404 Error" src={errorImg} />
           </Paper>
         </Container>
       </PrimaryLayout>
