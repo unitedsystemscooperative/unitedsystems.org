bb798a4df82cfb8664db57e2d36f72650ff37f3d
Zachary Austin <Zachary@admiralfeb.net>
2021-09-26 12:56:04 -0500

    refactor: mv Page content to components

---
+++ src/components/information/shipReviews.tsx
@@ -0,0 +1,51 @@
+import {
+  Button,
+  Container,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material';
+import { useShipReviews } from 'hooks/information/useShipReviews';
+
+export const ShipReviews = () => {
+  const shipReviews = useShipReviews();
+  return (
+    <Container maxWidth="sm" sx={{ textAlign: 'center' }}>
+      <Typography variant="h3">Ship Reviews</Typography>
+      <TableContainer component={Paper}>
+        <Table sx={{ textAlign: 'center' }}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Ship</TableCell>
+              <TableCell>Manufacture</TableCell>
+              <TableCell>Review Link</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {shipReviews.map((review) => (
+              <TableRow key={review.shipId}>
+                <TableCell>{review.name}</TableCell>
+                <TableCell>{review.manufacturer}</TableCell>
+                <TableCell>
+                  <Button
+                    href={review.shipReview}
+                    target="_blank"
+                    variant="contained"
+                    color="secondary"
+                  >
+                    Pilot Review
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+  );
+};
--- src/pages/information/ship-reviews.tsx
+++ src/pages/information/ship-reviews.tsx
@@ -1,59 +1,15 @@
-import {
-  Button,
-  Container,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Typography,
-} from '@mui/material';
-import { useShipReviews } from 'hooks/information/useShipReviews';
+import { ShipReviews } from 'components/information/shipReviews';
 import Head from 'next/head';
 
 /** Displays the ship Review Table */
 const ShipReviewsPage = () => {
-  const shipReviews = useShipReviews();
   return (
     <>
       <Head>
         <title>USC Ship Reviews</title>
         <meta name="description" content="Ship Reviews compiled by USC" />
       </Head>
-      <Container maxWidth="sm" sx={{ textAlign: 'center' }}>
-        <Typography variant="h3">Ship Reviews</Typography>
-        <TableContainer component={Paper}>
-          <Table sx={{ textAlign: 'center' }}>
-            <TableHead>
-              <TableRow>
-                <TableCell>Ship</TableCell>
-                <TableCell>Manufacture</TableCell>
-                <TableCell>Review Link</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {shipReviews.map((review) => (
-                <TableRow key={review.shipId}>
-                  <TableCell>{review.name}</TableCell>
-                  <TableCell>{review.manufacturer}</TableCell>
-                  <TableCell>
-                    <Button
-                      href={review.shipReview}
-                      target="_blank"
-                      variant="contained"
-                      color="secondary"
-                    >
-                      Pilot Review
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Container>
+      <ShipReviews />
     </>
   );
 };
