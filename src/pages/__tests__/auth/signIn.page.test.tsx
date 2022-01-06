4f8403cbeb3140ff4fb98f5bd79296b239ae15eb
Zachary Austin <Zachary@admiralfeb.net>
2022-01-05 21:09:06 -0600

    tests: add signIn getserversideprops test

--- src/pages/__tests__/auth/signIn.page.test.tsx
+++ src/pages/__tests__/auth/signIn.page.test.tsx
@@ -1,9 +1,9 @@
-import SignInPage from '@/pages/auth/signIn.page';
-import * as page from '@/pages/auth/signIn.page';
-import { render, fireEvent, waitFor } from '@testing-library/react';
+import SignInPage, { getServerSideProps } from '@/pages/auth/signIn.page';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import * as authLib from 'next-auth/react';
 
 const signInSpy = jest.spyOn(authLib, 'signIn').mockImplementation(jest.fn());
+const getSessionSpy = jest.spyOn(authLib, 'getSession').mockImplementation(jest.fn());
 
 describe('SignIn Page', () => {
   it('should render', () => {
@@ -33,5 +33,44 @@ describe('SignIn Page', () => {
       },
       { timeout: 500 }
     );
+  });
+
+  describe('getServerSideProps', () => {
+    it('should return empty if not logged in', async () => {
+      getSessionSpy.mockResolvedValue(undefined);
+
+      const result = await getServerSideProps({
+        req: null,
+        res: null,
+        query: { redirect: 'admin_index' },
+        resolvedUrl: '',
+      });
+
+      expect(result).toEqual({ props: {} });
+    });
+    it('should return the redirect if logged in', async () => {
+      getSessionSpy.mockResolvedValue({ expires: '500' });
+
+      const result = await getServerSideProps({
+        req: null,
+        res: null,
+        query: { redirect: 'admin_index' },
+        resolvedUrl: '',
+      });
+
+      expect(result).toEqual({ redirect: { destination: '/admin', permanent: false } });
+    });
+    it('should return the redirect to home if redirect is not found if logged in', async () => {
+      getSessionSpy.mockResolvedValue({ expires: '500' });
+
+      const result = await getServerSideProps({
+        req: null,
+        res: null,
+        query: { redirect: 'test_redirect' },
+        resolvedUrl: '',
+      });
+
+      expect(result).toEqual({ redirect: { destination: '/home', permanent: false } });
+    });
   });
 });
