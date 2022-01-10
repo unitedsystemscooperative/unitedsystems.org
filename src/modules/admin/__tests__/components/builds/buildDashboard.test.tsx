b95d4587d938421f9eccb021097d055523d092d4
Zachary Austin <Zachary@admiralfeb.net>
2022-01-10 05:06:58 +0000

    tests: add placeholders for other admin tests

---
+++ src/modules/admin/__tests__/components/builds/buildDashboard.test.tsx
@@ -0,0 +1,38 @@
+import { allies } from '@/modules/about/data/allies';
+import { server } from '@/__mocks__/server/server';
+import { SWRConfigReset } from '@/__mocks__/swr-reset';
+import { render, waitFor } from '@testing-library/react';
+import { rest } from 'msw';
+import { AllyDashboard } from '../../../components/allies/allyDashboard';
+
+describe('Admin Ally Dashboard', () => {
+  it('should render with loading', () => {
+    const { queryByTestId } = render(
+      <SWRConfigReset>
+        <AllyDashboard />
+      </SWRConfigReset>
+    );
+    expect(queryByTestId('allies-list')).toBeNull();
+  });
+
+  it('should render with data', async () => {
+    server.use(rest.get('*', (req, res, ctx) => res(ctx.status(200), ctx.json(allies))));
+
+    const { queryByTestId, getByText } = render(
+      <SWRConfigReset>
+        <AllyDashboard init={null} />
+      </SWRConfigReset>
+    );
+
+    await waitFor(() => {
+      expect(queryByTestId('allies-list')).toBeTruthy();
+      expect(getByText('ROOKS')).toBeTruthy();
+    });
+  });
+
+  it.todo('should add an ally');
+
+  it.todo('should edit an ally');
+
+  it.todo('should delete an ally');
+});
---
+++ src/modules/admin/__tests__/components/join/joinDashboard.test.tsx
@@ -0,0 +1,12 @@
+import { server } from '@/__mocks__/server/server';
+import { SWRConfigReset } from '@/__mocks__/swr-reset';
+import { render, waitFor } from '@testing-library/react';
+import { rest } from 'msw';
+
+describe('Admin Join Dashboard', () => {
+  it.todo('should render with loading');
+
+  it.todo('should render with data');
+
+  it.todo('should add an member to cmdr members');
+});
