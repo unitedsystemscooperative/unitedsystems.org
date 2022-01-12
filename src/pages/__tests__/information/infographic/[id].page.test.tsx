5328870ca6c552d8c08d759deae31f65d0eda4ef
Zachary Austin <Zachary@admiralfeb.net>
2022-01-11 19:26:28 -0600

    tests: add infographic tests

--- src/pages/__tests__/information/infographic/[id].page.test.tsx
+++ src/pages/__tests__/information/infographic/[id].page.test.tsx
@@ -1,6 +1,10 @@
 import { infoGraphics } from '@/modules/information/data';
 import { Infographic } from '@/modules/information/models/infographic';
-import { getStaticPaths, getStaticProps } from '@/pages/information/infographic/[id].page';
+import InfographicPage, {
+  getStaticPaths,
+  getStaticProps,
+} from '@/pages/information/infographic/[id].page';
+import { render } from '@testing-library/react';
 
 describe('Infographic Pages', () => {
   it('should generate a list of infographics for routing', () => {
@@ -16,5 +20,14 @@ describe('Infographic Pages', () => {
     const result = getStaticProps({ params: { id: 'cave-johnson' } })['props'] as {
       infographic: Infographic;
     };
+    const { getByText } = render(<InfographicPage infographic={result.infographic} />);
+    expect(getByText('Cave Johnson')).toBeTruthy();
+  });
+  it('should render not found', () => {
+    const result = getStaticProps({ params: { id: 'whoops' } })['props'] as {
+      infographic: Infographic;
+    };
+    const { getByText } = render(<InfographicPage infographic={result.infographic} />);
+    expect(getByText('Image not found'));
   });
 });
