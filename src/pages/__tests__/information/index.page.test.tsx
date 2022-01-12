6f15cde5f735bffe493f8660531f26bafdea9812
Zachary Austin <Zachary@admiralfeb.net>
2022-01-11 18:01:22 -0600

    tests: add more info tests

--- src/pages/__tests__/information/index.page.test.tsx
+++ src/pages/__tests__/information/index.page.test.tsx
@@ -1,9 +1,15 @@
 import InformationPage from '@/pages/information/index.page';
+import { theme } from '@/styles/theme';
+import { ThemeProvider } from '@mui/material';
 import { render } from '@testing-library/react';
 
 describe('Information Page', () => {
-  xit('should render', () => {
-    const component = render(<InformationPage />);
+  it('should render', () => {
+    const component = render(
+      <ThemeProvider theme={theme}>
+        <InformationPage />
+      </ThemeProvider>
+    );
     expect(component).toBeTruthy();
   });
 });
